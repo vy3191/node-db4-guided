ead9bc8314a0d6dc27bd0bda3418e7f32b64c99e
vy3191 <venkyafhs@gmail.com>
2020-01-12 16:47:22 -0500

    Modifed migrations

---
+++ data/migrations/20200108203020_initial.js
@@ -0,0 +1,36 @@
+
+exports.up = async function(knex) {
+  await knex.schema.createTable("zoos", (table) => {
+     table.increments("id");
+     table.string("name").notNullable();
+     table.string("address").notNullable();
+  });
+
+  await knex.schema.createTable("species", (table) => {
+    table.increments("id");
+    table.string("name").notNullable();
+
+ });
+
+  await knex.schema.createTable("animals", (table) => {
+     table.increments("id");
+     table.string("name").notNullable();
+     table.integer("species_id").notNullable().references("id").inTable("species");
+  })
+  
+  await knex.schema.createTable("zoos_animals", (table) => {
+     table.integer("zoo_id").notNullable().references("id").inTable("zoos");
+     table.integer("animal_id").notNullable().references("id").inTable("animals")
+     table.date("from")
+     table.date("to")
+     //create a primary key as a combination of columns
+     table.primary(["zoo_id", "animal_id"])
+  })
+ 
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("animals");
+   await knex.schema.dropTableIfExists("species");   
+   await knex.schema.dropTableIfExists("zoos");
+};
